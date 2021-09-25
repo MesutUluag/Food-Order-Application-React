7d99c4d5293dcb42b8c1944b8f83eb602c43c4a1
mesut uluag <punk7119@gmail.com>
2021-09-25 15:46:16 +0300

    adding element to cart logic - simplier version

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,25 @@
+import {useRef, useState} from "react";
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
 
-const MealItemForm = () =>{
-    return <form className={classes.form}>
-        <Input label="Amount" input={{id: 'amount', type:'number', min:'1', max:'5', step:'1', defaultValue:'1'}}/>
+const MealItemForm = (props) =>{
+    const amountInputRef = useRef();
+    const [amountIsValid, setAmountIsValid] = useState(true);
+
+    const submitHandler = event =>{
+        event.preventDefault(); // to make sure that browser reload is prevented
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+        if(enteredAmount.trim().length === 0 || enteredAmountNumber<1 || enteredAmountNumber>5){
+            setAmountIsValid(false);
+            return;
+        }
+        props.onAddToCart(enteredAmountNumber);
+    };
+    return <form className={classes.form} onSubmit={submitHandler}>
+        <Input ref={amountInputRef} label="Amount" input={{id: 'amount', type:'number', min:'1', max:'5', step:'1', defaultValue:'1'}}/>
         <button>+ Add</button>
+        {!amountIsValid && <p>Please enter a valid amount 1-5</p>}
     </form>
 };
 export default MealItemForm;
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+// we are using forwardRef to use useRef hook
+const Input = React.forwardRef((props, ref) => {
     return (
         <div className={classes.input}>
             <label htmlFor={props.input.id}>{props.label}</label>
-            <input {...props.input}/>
+            <input ref={ref} {...props.input}/>
         </div>
     );
-};
+});
 export default Input;
