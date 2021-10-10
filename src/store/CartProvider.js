def894633a10c20d8344c758ccb95d8e6383e8a2
mesut uluag <punk7119@gmail.com>
2021-10-10 12:04:28 +0300

    add only amount if there is an item at chart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,8 +8,22 @@ const defaultCartState ={
 
 const cartReducer = (state, action) =>{
     if (action.type === 'ADD'){
-        const updatedItems = state.items.concat(action.item);
         const updatedTotalAmount = state.totalAmount + action.item.price*action.item.amount;
+
+        const existingCartItemsIndex = state.items.findIndex(item => item.id === action.item.id);
+        const existingCartItem = state.items[existingCartItemsIndex];
+        let updatedItems;
+
+        if(existingCartItem){
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemsIndex] = updatedItem;
+        } else {
+            updatedItems = state.items.concat(action.item);
+        }
         return {
           items: updatedItems,
           totalAmount: updatedTotalAmount
