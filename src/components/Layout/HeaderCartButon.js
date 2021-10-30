41f048259596ff0fa46f809d21a90286ab9d3bbe
mesut uluag <punk7119@gmail.com>
2021-10-30 09:33:26 +0300

    update chart amount with animation when user adds new amount

--- src/components/Layout/HeaderCartButon.js
+++ src/components/Layout/HeaderCartButon.js
@@ -1,14 +1,36 @@
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
-const HeaderCartButon = props =>{
+const HeaderCartButon = props => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false)
     const cartCtx = useContext(CartContext);
-    const numberOfCartItems = cartCtx.items.reduce((curNumber,item)=>{return curNumber+item.amount},0);
+    const {items} = cartCtx;
+    const numberOfCartItems = items.reduce((curNumber, item) => {
+        return curNumber + item.amount
+    }, 0);
 
-    return <button className={classes.button} onClick={props.onClick}>
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        // if you return a function in useEffect it will be a clean-up function
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
+    return <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon/>
         </span>
