201501c4cf96f19f1a8979a722b2f5cdf2857fe8
mesut uluag <punk7119@gmail.com>
2021-11-07 12:44:31 +0300

    add checkout module - initial version

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,12 @@
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
 import CartItem from "./CartItem";
+import Checkout from "./Checkout";
 
 const Cart = (props) => {
+    const [isCheckout, setIsCheckout] = useState(false);
     const cartCtx = useContext(CartContext);
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItems = cartCtx.items.length > 0;
@@ -16,6 +18,10 @@ const Cart = (props) => {
         cartCtx.addItem(item);
     };
 
+    const orderHandler = () => {
+        setIsCheckout(true);
+    };
+
     const cartItems = (
         <ul className={classes['cart-items']}>
             {cartCtx.items.map(
@@ -25,17 +31,24 @@ const Cart = (props) => {
                 ))}
         </ul>
     );
-    return <Modal onClose={props.onClose}>
-        {cartItems}
-        <div className={classes.total}>
-            <span>Total Amount</span>
-            <span>{totalAmount}</span>
-        </div>
+
+    const modalActions = (
         <div className={classes.actions}>
             <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-            {hasItems && <button className={classes.button}>Order</button>}
+            {hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}
         </div>
-    </Modal>
+    );
+    return (
+        <Modal onClose={props.onClose}>
+            {cartItems}
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>{totalAmount}</span>
+            </div>
+            {isCheckout && <Checkout onCancel={props.onClose}/>}
+            {!isCheckout && modalActions}
+        </Modal>
+    );
 };
 
 export default Cart;
